99f677015434f4f07f36e9b2aa7263bd7942a64f
Student <sharmaalson@gmail.com>
2025-03-21 17:10:53 -0400

    final commit

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -35,7 +35,7 @@ const login = async(req, res) => {
                 secure: true,
                 maxAge: 3600000, // 1 hour
                 sameSite: 'none',
-                domain: 'https://videxify-videoconferencingapp-1.onrender.com'
+                domain: 'videxify-videoconferencingapp-1.onrender.com'
             });
 
             return res.status(httpStatus.OK).json({message: "Login successfull"});
